6b26e18e8d47ff4cc82b4bc0d9de15c57fb92aa4
Masoud Amiri Batmanghlnj <49142630+WorldSimorgh@users.noreply.github.com>
2020-10-01 11:47:17 +0330

    Add files via upload

---
+++ uy.py
@@ -0,0 +1,11 @@
+import wikipediaapi
+input=input("SPage:")
+while True:
+    wi=wikipediaapi.Wikipedia('en')
+    wk=wi.page(input)
+    wks=wk.summary[0:244]
+    #wkutf=wks.encode()
+    print("Page Exists:%s"% wk.exists())
+    print("Title:",wk.title,"\nSummary:",wks,"...")
+    print("For more information, go to the link below.:\n",wk.fullurl)
+    break
