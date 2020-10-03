bd499117f148f6dd7855f7cb4dc0f544e840bd7b
Masoud Amiri Batmanghlnj <49142630+WorldSimorgh@users.noreply.github.com>
2020-10-03 11:22:36 +0330

    Update Wikipedia Search page.py
    Add: if and else
    Remove:print("Page Exists:%s"% wk.exists())

--- Wikipedia Search page.py
+++ Wikipedia Search page.py
@@ -4,8 +4,11 @@ def wiksp(str):
     wiks=input("SPage:")
     wi=wikipediaapi.Wikipedia('en')
     wk=wi.page(wiks)
+    if wk==True:
+                print("Page Exists:%s"% wk.exists())
+    else:
+                print("Page Exists:%s"% wk.exists())
     wks=wk.summary[0:244]
-    print("Page Exists:%s"% wk.exists())
     print("Title:",wk.title,"\nSummary:",wks,"...")
     print("For more information, go to the link below.:\n",wk.fullurl)
 wiksp("SPage:")
