85395fb7930a6d574b42c6b8b9c1d40680e3cb49
Masoud Amiri Batmanghlnj <49142630+WorldSimorgh@users.noreply.github.com>
2020-10-05 13:50:16 +0330

    Update Wikipedia Search page.py
    Add to:
    1. Repeat loop while
    2. print("-------------------")

--- Wikipedia Search page.py
+++ Wikipedia Search page.py
@@ -1,14 +1,16 @@
 #Wikipedia Search page
 import wikipediaapi
 def wiksp(str):
-    wiks=input("SPage:")
-    wi=wikipediaapi.Wikipedia('en')
-    wk=wi.page(wiks)
-    if wk==True:
-                print("Page Exists:%s"% wk.exists())
-    else:
-                print("Page Exists:%s"% wk.exists())
-    wks=wk.summary[0:244]
-    print("Title:",wk.title,"\nSummary:",wks,"...","\nFor more information, go to the link below.:\n",wk.fullurl)
+    while True:
+        wiks=input("SPage:")
+        wi=wikipediaapi.Wikipedia('en')
+        wk=wi.page(wiks)
+        if wk==True:
+                    print("Page Exists:%s"% wk.exists())
+        else:
+                    print("Page Exists:%s"% wk.exists())
+        wks=wk.summary[0:244]
+        print("Title:",wk.title,"\nSummary:",wks,"...","\nFor more information, go to the link below.:\n",wk.fullurl)
+        print("----------------------------------------------------------------------------------------------------------------")
 wiksp("SPage:")
 quit=input()
