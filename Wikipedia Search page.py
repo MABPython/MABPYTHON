14cc1f4cf5cc6f8c44e535fa6812bdc9c60f41a2
Masoud Amiri Batmanghlnj <49142630+WorldSimorgh@users.noreply.github.com>
2020-10-06 11:54:54 +0330

    Update Wikipedia Search page.py
    Add to:
    language Persian Added
    Combination:
    Combining line 14 with line 13

--- Wikipedia Search page.py
+++ Wikipedia Search page.py
@@ -10,7 +10,16 @@ def wiksp(str):
         else:
                     print("Page Exists:%s"% wk.exists())
         wks=wk.summary[0:244]
-        print("Title:",wk.title,"\nSummary:",wks,"...","\nFor more information, go to the link below.:\n",wk.fullurl)
-        print("----------------------------------------------------------------------------------------------------------------")
+        print("Title:",wk.title,"\nSummary:",wks,"...","\nFor more information, go to the link below.:\n",wk.fullurl,"\n----------------------------------------------------------------------------------------------------------------")
+        #Persian
+        wikp=wikipediaapi.Wikipedia('fa')
+        wkp=wikp.page(wiks)
+        if wkp==True:
+                    print("Page Exists:%s"% wkp.exists())
+        else:
+                    print("Page Exists:%s"% wkp.exists())
+        wksp=wkp.summary[0:244]
+        
+        print("عنوان:",wkp.title,"\nتوضحات:\n",wksp,"...","\nپیوند.:\n",wkp.fullurl,"\n----------------------------------------------------------------------------------------------------------------")
 wiksp("SPage:")
 quit=input()
