2925e62dd55fbbb0e2b5145a81a7ea19d8007219
Masoud Amiri Batmanghlnj <49142630+WorldSimorgh@users.noreply.github.com>
2020-11-06 12:14:57 +0330

    Persian language was deleted(New version of the program in Persian)
    Persian language was deleted
    It is true that Persian language was removed from the program, but it has become a separate program.
    link:
    https://github.com/WikipediaSearchPage/Wikipedia-Search-Page/blob/master/Wikipedia%20Search%20page(Persian).py

--- Wikipedia Search page.py
+++ Wikipedia Search page.py
@@ -10,5 +10,16 @@ def wiksp(str):
         else:
                     print("Page Exists:%s"% wk.exists())
         wks=wk.summary[0:244]
-        print("Title:",wk.title,"\nSummary:",wks,"...","\nFor more information, go to the link below.:\n",wk.fullurl,)
+        print("Title:",wk.title,"\nSummary:",wks,"...","\nFor more information, go to the link below.:\n",wk.fullurl,"\n------------------------------------------------------Persian----------------------------------------------------------")
+        #Persian
+        wikp=wikipediaapi.Wikipedia('fa')
+        wkp=wikp.page(wiks)
+        if wkp==True:
+                    print("Page Exists:%s"% wkp.exists())
+        else:
+                    print("Page Exists:%s"% wkp.exists())
+        wksp=wkp.summary[0:244]
+        
+        print("عنوان:",wkp.title,"\nتوضحات:\n",wksp,"...","\nپیوند.:\n",wkp.fullurl,"\n----------------------------------------------------------------------------------------------------------------")
 wiksp("SPage:")
+quit=input()
